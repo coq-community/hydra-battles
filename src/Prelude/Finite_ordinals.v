9eb9d90111b65f922212ebb9d9cf724028b56f1a
Pierre Casteran <pierre.casteran@labri.fr>
2020-07-03 17:52:34 +0200

    redaction Finite_ordinals

--- src/Prelude/Finite_ordinals.v
+++ src/Prelude/Finite_ordinals.v
@@ -1,8 +1,7 @@
-Require Import Arith Relations Lia Logic.Eqdep_dec.
+Require Import Arith Relations Lia Logic.Eqdep_dec
+        Coq.Wellfounded.Inverse_Image Coq.Wellfounded.Inclusion.
+Require        Wf_nat.
 Coercion is_true: bool >-> Sortclass.
-
-
-Search (nat->nat->bool).
 
 Definition t (n:nat) := {i:nat | Nat.ltb i  n}.
 
@@ -15,77 +14,104 @@ Proof.
   destruct x; cbn in i; discriminate.
 Qed.
 
-Definition compare {n:nat} (alpha beta : t (S n)) :=
+Definition compare {n:nat} (alpha beta : t n) :=
   Nat.compare (proj1_sig alpha) (proj1_sig beta).
 
-(** Too long ! *)
+Lemma compare_ok {n} (alpha beta : t n) :
+  CompareSpec (alpha = beta) (lt alpha beta) (lt beta alpha)
+              (compare alpha beta).
+Proof.
+  destruct n. 
+  - elimtype False;  now apply t0_empty.
+  - destruct alpha, beta; cbn;  case_eq (x ?= x0); unfold lt; cbn.
+    + rewrite Nat.compare_eq_iff; intro; subst; f_equal. 
+      constructor 1; f_equal; apply eq_proofs_unicity_on.
+      destruct y, (x0 <? S n); auto; right; discriminate.    
+    +  rewrite Nat.compare_lt_iff;  constructor 2.
+       destruct x0; [  lia |  apply leb_correct; lia].
+    +   rewrite Nat.compare_gt_iff; constructor 3.
+        destruct x; [lia | apply leb_correct; lia].
+Qed.
 
-Lemma compare_reflect {n:nat} (alpha beta : t (S n)) :
+(* useful ? *)
+
+
+Lemma compare_reflect {n:nat} (alpha beta : t  n) :
   match (compare alpha beta)
   with
     Lt => lt alpha  beta
   | Eq => alpha = beta
   | Gt => lt beta  alpha
   end.
-
-  destruct alpha, beta; cbn. 
-  case_eq (x ?= x0); cbn.
-  rewrite Nat.compare_eq_iff. intro; subst.
-  f_equal;   apply eq_proofs_unicity_on.
-  destruct (x0 <? S n),y; auto. right; discriminate.
-  unfold lt;cbn.  
-  destruct x0.
-  destruct x; cbn.
-  discriminate.
-  discriminate.
-  rewrite Nat.compare_lt_iff .
-  Search (_ <=? _).
-  intro; apply leb_correct.
-  lia.
-  intro.
-  unfold lt;cbn.  
-  destruct x; cbn.
-  destruct x0.
-  discriminate.
-  discriminate.
-  rewrite Nat.compare_gt_iff in H.
-  apply leb_correct.
-  lia.
+Proof.
+ now destruct (compare_ok alpha beta).
 Qed.
 
 
-(** Examples *)
 
-Program Definition alpha : t 10 := 5.
 
-Program Definition beta : t 10 := 8.
-
-Goal lt  alpha beta.
-  now compute. 
+Lemma lt_wf (n:nat) : well_founded (@lt n).
+Proof.
+  intro x; apply Acc_incl with (fun alpha beta =>
+                                  proj1_sig alpha < proj1_sig beta)%nat.
+ - intros  [y Hy] [z Hz]; unfold lt; cbn; intro H.
+   destruct z.
+   + discriminate.
+   +  apply leb_complete in H; lia.
+  -  apply Acc_inverse_image, Wf_nat.lt_wf.
 Qed.
 
-Program Definition inj {i : nat} (j:nat) (H: i <= j) (alpha: t i) : t j :=
-   alpha.
+
+
+Program Definition cast {i : nat} (j:nat) (H: i <= j) (alpha: t i) : t j :=
+  alpha.
+
 Next Obligation.
-  destruct alpha0. cbn.
-   Search (_ <? _).
-   
-red in i0;rewrite  Nat.ltb_lt  in i0.
-destruct j.
-lia.
-apply leb_correct.
- lia.
+  destruct alpha; cbn; red in i0;rewrite  Nat.ltb_lt  in i0.
+  destruct j; [lia | apply leb_correct;  lia].
 Qed. 
 
 
-Remark r12: (10 <= 12)%nat.
-  lia.
+Lemma cast_compare_commute (i j  :nat)(H : i <= j) :
+  forall alpha beta, compare alpha beta = compare (cast j H alpha)
+                                                  (cast j H beta).
+  reflexivity. 
 Qed.
-Check inj  12  r12 beta.
-
-Lemma inj_ok (i j  :nat)(H : i <= j) : forall alpha beta,
+  
+Lemma cast_mono (i j  :nat)(H : i <= j) : forall alpha beta,
     lt alpha beta <->
-    lt (inj j H alpha) (inj j H beta).
-  split;  unfold lt; simpl; auto.
+    lt (cast j H alpha) (cast j H beta).
+Proof.
+  split;  unfold lt; cbn; auto.
 Qed.
+
+
+
+(** Examples:   5 and 8 considered as members of the segma,t [0,10[ *)
+
+Program Example alpha1 : t 7 := 2.
+
+Program Example beta1 : t 7 := 5.
+
+Example i1 : lt  alpha1 beta1.
+Proof.   now compute. Qed.
+
+Program Example gamma1 : t 8 := 7.
+
+Fail Goal lt alpha1 gamma1.
+
+
+
+(** Hide before compiling the module 
+
+Program Definition bad : t 10 := 10.
+Next Obligation.
+  compute.
+Abort.
+ **)
+
+
+
+
+
 
