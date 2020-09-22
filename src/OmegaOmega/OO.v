c745711d5f8a504f8e1d86f53eb9a125bfaf6893
Pierre Casteran <pierre.casteran@labri.fr>
2020-09-22 13:57:36 +0200

    new module OmegaOmega.OO

---
+++ src/OmegaOmega/OO.v
@@ -0,0 +1,26 @@
+Require Import ListOmega.
+From Coq Require Import  Logic.Eqdep_dec.
+
+Definition t := {l: t | nf l}.
+
+Search sig.
+
+Lemma compare_eq_nf  (l l':list nat) :
+  nf l -> nf l' -> ListOmega.compare l l' = Eq -> l = l'.
+Admitted.
+
+Definition compare (alpha beta:t) := ListOmega.compare (proj1_sig alpha)
+                                                       (proj1_sig beta).
+
+Lemma compare_eq_eq  alpha beta  : compare alpha beta = Eq -> alpha = beta.
+Proof.
+  destruct alpha, beta; unfold compare;cbn.
+  intro.
+  assert (x = x0) . { eapply compare_eq_nf; eauto. }
+  subst.
+  f_equal.
+  apply eq_proofs_unicity_on.
+  destruct y, (nf x0); (auto ||  (right; discriminate)).
+Qed.
+
+
