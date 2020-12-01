ba784ef5d09c70ba5ff0fba9559a4e7708b14c2b
Pierre Casteran <pierre.casteran@labri.fr>
2020-12-01 21:28:09 +0100

    Yves example with chains

--- theories/additions/Fib2.v
+++ theories/additions/Fib2.v
@@ -1,3 +1,5 @@
+
+
 Require Import NArith Ring Monoid_instances Euclidean_Chains Pow AM
         Strategies Dichotomy.
 
@@ -8,13 +10,29 @@ Open Scope N_scope.
 Fixpoint fib (n:nat) : N :=
   match n with
     0%nat | 1%nat => 1%N
-  | (S (S p as q)) => (fib p + fib q)%N
+  | (S ((S p) as q)) => (fib p + fib q)%N
   end.
 
-(** Yves' encoding *)
 
-Definition phi (p:N * N) : M2 N :=
-  let (a,b) := p in Build_M2 (a+b) a a b.
+Lemma fib_ind (P:nat->Prop) :
+  P 0%nat -> P 1%nat -> (forall n, P n -> P (S n) -> P(S (S n))) ->
+  forall n, P n.
+intros.
+assert (P n /\ P (S n)).
+{ induction n.
+  split;auto.
+   destruct IHn; split; auto.
+}
+ tauto.
+Qed.
+
+Lemma fib_SSn : forall (n:nat) , fib (S (S n)) = (fib n + fib (S n))%N.
+Proof.
+  intro n; pattern n; apply fib_ind; try reflexivity.
+Qed.
+
+
+(** Yves' encoding *)
 
 
 Definition mul2 (p q : N * N) :=
@@ -34,9 +52,45 @@ Proof.
   destruct x,y,z; unfold mul2;  cbn; f_equal; ring.
   intro x; now rewrite neutral_l.
   intro  x; now rewrite neutral_r.
-Qed. 
+Qed.
 
-Existing Instance M2N.
+Lemma next_fib (n:nat) : mul2 (1,0)%N (fib (S n), fib n) =
+                         (fib (S (S n)), fib (S n)).
+unfold mul2.
+f_equal; ring_simplify.
+-  rewrite fib_SSn. ring.
+- reflexivity.
+Qed.
+
+
+
+Lemma fib_power_0 (n:nat) : power (M:=Mul2) (1,0)%N (S (S n)) =
+                          (fib (S n), fib n).
+
+Proof.
+  induction n.
+  - reflexivity. 
+  - now rewrite power_eq2, IHn, next_fib.
+Qed.
+
+Compute  power (M:=Mul2) (1,0)%N 0%nat.
+Compute  power (M:=Mul2) (1,0)%N 1%nat.
+
+
+Lemma fib_power n : fib n = let (a,b) := power (M:=Mul2) (1,0)%N n
+                            in (a+b)%N.
+Proof.
+pattern n;apply fib_ind; try reflexivity.
+- intros; rewrite fib_power_0; now rewrite fib_SSn, N.add_comm.
+Qed.
+
+(** Connexion with matrices  (for documentation only) *)
+
+
+Definition phi (p:N * N) : M2 N :=
+  let (a,b) := p in Build_M2 (a+b) a a b.
+
+
 
 Lemma phi_morph (p q : N * N) :
   phi (mul2 p q) == (M2_mult N (N.add) (N.mul) (phi p) (phi q))%M.
@@ -53,13 +107,15 @@ Lemma phi_morph (p q : N * N) :
    -  cbn; intro x;  rewrite phi_morph.
       unfold mult_op,  M2_op; now rewrite IHn. 
   Qed.
-  
+
+  (** End of matrices *)
 
 
 Definition fib_pos n :=
   let (a,b) := Pos_bpow (M:= Mul2) (1,0)%N n in
   (a+b)%N.
 
+Compute fib_pos xH.
 Compute fib_pos 10%positive. 
 
 Compute fib_pos 153%positive.
