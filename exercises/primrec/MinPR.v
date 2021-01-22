2729f3c37426aba23c9fd0bd9df6b5e491e905b3
Pierre Casteran <pierre.casteran@labri.fr>
2021-01-22 20:44:47 +0100

    correction of an exercise

--- exercises/primrec/MinPR.v
+++ exercises/primrec/MinPR.v
@@ -5,27 +5,13 @@
 From hydras Require Import  primRec extEqualNat.
 From Coq Require Import Min.
 
-(** Define an n-ary if-then-else *)
-
-Parameter naryIf :
-  forall (n: nat), naryRel n -> naryFunc n -> naryFunc n -> naryFunc n.
-
-(** Prove a few useful lemmas about [naryIf] *)
 
 Section Proof_of_MinIsPR.
 
-Let minPR : naryFunc 2 :=
-  naryIf 2 leBool (fun x _ => x) (fun _ y => y).
-
-(** Note : If you failed to define a useful [naryIf], 
-   you may use arithmetic operations instead.
-
-<<
 Let minPR (a b: nat) : nat :=
   (charFunction 2 leBool a b) * a +
   (charFunction 2 ltBool b a) * b.
->>
-*)
+
 
 Lemma minPR_correct : extEqual 2 minPR min.
 Admitted.
@@ -43,5 +29,6 @@ Qed.
 
 
 End Proof_of_MinIsPR.
+
 
 
