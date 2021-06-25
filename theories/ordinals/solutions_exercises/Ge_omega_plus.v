45a868885dcbad73e905be14b1c09368eefdc947
jeremy damour <jeremy.damour2@gmail.com>
2021-06-25 21:01:37 +0000

    Fix Ge_omega_plus exercice file

--- theories/ordinals/solutions_exercises/Ge_omega_plus.v
+++ theories/ordinals/solutions_exercises/Ge_omega_plus.v
@@ -6,28 +6,25 @@ Lemma ge_omega_iff (alpha : E0):
   omega o<= alpha <-> (forall i:nat, i + alpha = alpha).
 Proof.
   destruct alpha as [a Ha];  unfold Le;  cbn.
-  -   destruct a; cbn.
-      + split.
-        *   unfold le; cbn;  intro H; destruct H; destruct H0.
-            specialize (le_zero_inv _ H0); discriminate.            
-        *  intros; specialize (H 1); compute in H;
-           injection H; discriminate.
-      +  split; intros.
-        * cbn; apply E0_eq_intro; cbn.
-          destruct i; cbn; auto.
-          --  destruct a1.
-              ++  destruct H as [H [H0 H1]]; destruct H0.
-              ++   auto.
-        *   intros;  destruct a1.
-        --   specialize (H 1); cbn in H.
-             assert (a2 = zero) by (eapply nf_of_finite; eauto).
-             subst; compute in H; inversion H; lia.
-        --  repeat split;  auto with T1.    
-            apply le_trans with (phi0 (ocons a1_1 n0 a1_2)).
-             ++ apply phi0_mono.
-                apply le_trans with (ocons a1_1 0 zero); auto with T1.
-                **  apply phi0_mono; auto with T1.
-                ** apply le_phi0.   
-             ++ apply le_phi0.
+  destruct a; cbn; split; intro H.
+  - destruct H, H0.
+    now apply le_zero_inv in H0.
+  - specialize (H 1); compute in H;
+    injection H; discriminate.
+  - intro i. apply E0_eq_intro. cbn.
+    destruct i; cbn; auto.
+    destruct a1; auto.
+    now destruct H as [_ [[Heq | Hlt] _]].
+  - intros; destruct a1.
+    + specialize (H 1); cbn in H.
+      assert (a2 = zero) by (eapply nf_of_finite; eauto).
+      subst; compute in H; inversion H; lia.
+    + repeat split;  auto with T1.
+      apply le_trans with (phi0 (ocons a1_1 n0 a1_2)).
+      * apply phi0_mono.
+        apply le_trans with (ocons a1_1 0 zero).
+        -- apply phi0_mono; auto with T1.
+        -- apply le_phi0.
+      * apply le_phi0.
 Qed.
 
