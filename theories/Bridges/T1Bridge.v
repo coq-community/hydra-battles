2d40b40f1566fbc2c7aa0b180455d105c1bf62db
Pierre Casteran <pierre.casteran@labri.fr>
2021-08-13 17:05:49 +0200

    Bridges: first steps

--- theories/Bridges/T1Bridge.v
+++ theories/Bridges/T1Bridge.v
@@ -1,10 +1,12 @@
 (** Comparison between Hydra-battle's and Gaia's [T1]  
 
 First experiments *)
-
-
-From hydras Require  T1.
 From gaia Require Import ssete9.
+
+From Coq Require Import Arith Lia.
+From hydras Require  T1.
+
+Set Bullet Behavior "Strict Subproofs".
 
 Coercion is_true: bool >-> Sortclass.
 
@@ -79,4 +81,71 @@ Proof.
        (destruct alpha2; [trivial | discriminate]).
        subst; constructor.
 Qed.
+
+Lemma T1eq_refl a : T1eq a a.
+Proof.
+  destruct (@T1eqP a a).  
+  - trivial.
+  - now destruct n.
+Qed.
+
+Lemma ssrnat_eqn_refl n : ssrnat.eqn n n.
+Proof.
+ Search ssrnat.eqn .
+ destruct (@ssrnat.eqnP n n).
+ - trivial.    
+ - now destruct n0.
+Qed. 
+
+(* Ugly !!! I should learn ssr ! *)
+
+Lemma compare_ref x  :
+  forall y,
+    match T1.compare x y with
+    | Lt => T1lt (iota x) (iota y)
+    | Eq => iota x = iota y
+    | Gt => T1lt (iota y) (iota x)
+    end.
+Proof.
+  induction  x.  
+  - destruct y.
+    + reflexivity.
+    + now cbn.
+  - destruct y.
+    + now cbn.
+    + cbn.
+      * case_eq (T1.compare x1 y1); intro H.
+        -- specialize (IHx1 y1);  rewrite H in IHx1 .
+           case_eq ( PeanoNat.Nat.compare n n0 ); intro H0.
+           ++ assert (n = n0) by
+                 (now apply Compare_dec.nat_compare_eq); subst n0.
+              ** case_eq (T1.compare x2 y2); intro H1.
+                 rewrite IHx1; f_equal.
+                 specialize (IHx2 y2); now rewrite H1 in IHx2.
+                 case (iota x1 < iota y1); [trivial|].
+                 rewrite IHx1, T1eq_refl.
+                 case (ssrnat.eqn (ssrnat.subn_rec (S n) n) 0); trivial.
+                 rewrite ssrnat_eqn_refl.
+                 specialize (IHx2 y2); now rewrite H1 in IHx2.
+                 rewrite IHx1. rewrite T1ltnn, T1eq_refl. 
+                 case ( ssrnat.eqn (ssrnat.subn_rec (S n) n) 0);[trivial|].
+                 rewrite ssrnat_eqn_refl.
+                 specialize (IHx2 y2). now rewrite H1 in IHx2.  
+           ++ rewrite IHx1, T1ltnn, T1eq_refl.
+              destruct (@ssrnat.eqnP (ssrnat.subn_rec (S n) n0) 0).
+              ** now destruct (ssrnat.eqn (ssrnat.subn_rec (S n) n0) 0).
+              ** destruct n1.
+                 rewrite  <- ssrnat.subnE, <- ssrnat.minusE.
+                 apply nat_compare_Lt_lt in H0; lia.
+           ++ rewrite IHx1, T1ltnn, T1eq_refl; apply nat_compare_Gt_gt in H0.
+              rewrite  <- ssrnat.subnE, <- ssrnat.minusE.
+              destruct (@ssrnat.eqnP (S n0 - n)%nat 0). 
+              ** trivial. 
+              ** lia. 
+        -- specialize (IHx1 y1); rewrite H in IHx1;  now rewrite IHx1.
+        -- specialize (IHx1 y1); rewrite H in IHx1; now rewrite IHx1.
+Qed. 
+
+
+
 
