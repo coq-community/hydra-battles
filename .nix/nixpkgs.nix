c39f47e0cdaab8ca9b1f7b5b6ba5723ef759dd71
Théo Zimmermann <theo.zimmermann@inria.fr>
2021-10-11 16:15:46 +0200

    Bump paramcoq to 1.1.3.

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-         url = https://github.com/Zimmi48/nixpkgs/archive/1092468cb7115327cbef339e3b1185b37fb375b0.tar.gz;
-         sha256 = "1c5d01qcjcsf01hww09xjz7wcsxsfqffczwrrz52v99c5y5d0lpm";
+         url = https://github.com/Zimmi48/nixpkgs/archive/451f727a09e43ba16021448a20e5ef8b20e8c421.tar.gz;
+         sha256 = "18h92bb4sijih1klivjg75r19wci6gbgasnc0d8mpq57yj7ma8ys";
        }
