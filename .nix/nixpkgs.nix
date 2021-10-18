87d8137deb8804c9b71ddaad82d1c6dc9020d39f
Théo Zimmermann <theo.zimmermann@inria.fr>
2021-10-18 12:10:55 +0200

    Move definition of gaia-hydras package to nixpkgs.

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-         url = https://github.com/Zimmi48/nixpkgs/archive/0300b6ef8480620bfa2d668cf19314df11507ab1.tar.gz;
-         sha256 = "12hw8sc4krqfxpc5pdb78ccnl824maxhl967yyvn5mql0mncd359";
+         url = https://github.com/Zimmi48/nixpkgs/archive/699d147fe8bb5e2964790d230f35258e33f1aa75.tar.gz;
+         sha256 = "0fq2yamrbag7x29d1qkqzz8j0bhm2dc6xvi24ahmy19rdd0lswbn";
        }
