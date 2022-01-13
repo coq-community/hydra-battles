81de75ff38fd2a8ac38c2c52b0aa1ce2fb24c367
Pierre Casteran <pierre.casteran@labri.fr>
2022-01-13 15:11:17 +0100

    additions to GCanon.V

--- theories/gaia/GCanon.v
+++ theories/gaia/GCanon.v
@@ -14,11 +14,23 @@ From hydras Require Import Canon.
 Definition gcanon (a: gT1) (i:nat) : gT1 :=
   iota (hcanon (pi a) i).
 
-
-
 Compute gcanon (g_phi0 T1omega) 6 == (g_phi0 (\F 6))%brg.
 
+Lemma gcanon_zero i:  gcanon g_zero i = g_zero. 
+Proof.  unfold gcanon => //. Qed. 
 
+Lemma nf_gcanon alpha i : g_nfb alpha -> g_nfb (gcanon alpha i).
+Proof.  
+ unfold gcanon; rewrite -nf_ref => Halpha; rewrite -(iota_pi alpha)in Halpha.
+ rewrite -nf_ref in Halpha; by  apply nf_canon. 
+Qed.
 
+Search hcanon.
 
+Lemma gcanon_succ i alpha: g_nfb alpha -> gcanon (g_succ alpha) i = alpha.
+Proof.
+  rewrite -(iota_pi alpha). rewrite succ_ref. rewrite -nf_ref.
+  unfold gcanon. rewrite pi_iota. Search hcanon h_succ.
+  move => Halpha; rewrite canon_succ => //.
+Qed.
 
