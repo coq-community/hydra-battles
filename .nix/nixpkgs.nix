7b015ba0f0d73cc547c60397237bdb2ab7ddfc35
Théo Zimmermann <theo.zimmermann@inria.fr>
2022-02-21 15:18:39 +0100

    Update Coq Nix Toolbox and remove hydra-battles overlay.
    Now, we use a version of nixpkgs that knows about the new LibHyps dependency.

--- .nix/nixpkgs.nix
+++
@@ -1,4 +0,0 @@
-fetchTarball {
-         url = https://github.com/Zimmi48/nixpkgs/archive/c9c002d30a165fe0ecfec348de66bbe910ac1a57.tar.gz;
-         sha256 = "05mx4mmw0px0sylal287clpj3s6gm2qy0djjaxxm71s06y2j5fv6";
-       }
