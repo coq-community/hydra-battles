827882ebbfea934bbfca5bea71be3c0a65b6a742
Théo Zimmermann <theo.zimmermann@inria.fr>
2022-02-21 16:30:58 +0100

    Remove hydra-battles overlay for good after nixpkgs fixup.

---
+++ .nix/nixpkgs.nix
@@ -0,0 +1,4 @@
+fetchTarball {
+    url = https://github.com/NixOS/nixpkgs/archive/61ac4169922ca62d00bfe7f28dae6e1f27fd9c89.tar.gz;
+    sha256 = "05rjb4xx2m2qqp94x39k8mv447njvyqv1zk6kshkg0j9q4hcq8lf";
+  }
