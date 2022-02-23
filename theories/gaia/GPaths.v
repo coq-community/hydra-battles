b69fa543e927f43a44312ccf35044ca455404d30
Pierre Casteran <pierre.casteran@labri.fr>
2022-02-23 10:42:47 +0100

    still porting theorems from Epsilon0.Paths to GPaths (gaia)

--- theories/gaia/GPaths.v
+++ theories/gaia/GPaths.v
@@ -43,11 +43,47 @@ Definition pathS (from: T1)(s: seq nat) (to: T1) : Prop :=
   hpathS (g2h from) s (g2h to).
 
 
+#[global] Notation hKP_arrow  := KP_arrow.
+
+Definition KP_arrow n :=
+  Relation_Operators.clos_trans_1n T1 (bounded_transition n).
+
+#[global] Notation hconst_pathS  := const_pathS.
+#[global] Notation hconst_path  := const_path.
+
+Definition const_pathS i alpha beta  :=
+  hconst_pathS i (g2h alpha) (g2h beta).
+
+
+
+Definition const_path i alpha beta :=
+   hconst_path i (g2h alpha) (g2h beta).
+
+
 #[global] Notation hgnawS := gnawS.
 #[global] Notation hgnaw := gnaw.
 
 Definition gnawS alpha s := h2g (hgnawS (g2h alpha) s).
 Definition gnaw alpha s := h2g (hgnaw (g2h alpha) s).
+
+#[global] Notation hstandard_gnaw := standard_gnaw.
+Definition standard_gnaw i alpha l := h2g (hstandard_gnaw i (g2h alpha) l).
+
+
+Lemma iota_adapt i l: iota i l = MoreLists.iota_from i l. 
+Proof.   elim: i => /= //. Qed. 
+
+
+Lemma standard_gnaw_iota_from i alpha l :
+  i <> 0 -> standard_gnaw i alpha l = gnaw alpha (iota i l).
+  Proof. 
+   move: l i alpha ; elim => // /=. 
+   rewrite !/standard_gnaw => n Hn i alpha Hi.
+   specialize (Hn (S i) (canon alpha i)); rewrite /canon g2h_h2gK in Hn.
+    rewrite Hn /gnaw ?g2h_h2gK => // /=.  
+    move: Hi Hn; case :i => //.  
+  Qed. 
+ 
 
 
 (** * Examples *)
@@ -65,10 +101,9 @@ Example ex_path4: path (T1omega * \F 2) (List.repeat 3 8) zero.
 Proof. rewrite /path_to => /=. path_tac. Qed.
 
 Example ex_path5: pathS (T1omega * \F 2) (List.repeat 2 8) zero.
-Proof.
-  rewrite /pathS path_toS_path_to => /=; path_tac.
-Qed.
+Proof. rewrite /pathS path_toS_path_to => /=; path_tac. Qed.
 
+(* 
 Compute ppT1 (gnaw (phi0 T1omega) [:: 2; 3; 4; 5]).
 
 Compute ppT1 (gnaw (phi0 T1omega) (index_iota 2 10)).
@@ -79,13 +114,27 @@ Compute ppT1 (gnaw (phi0 T1omega) (index_iota 2 38)).
 
 Compute ppT1 (gnawS (phi0 T1omega) (index_iota 1 37)).
 
+Compute ppT1 (standard_gnaw  2 (phi0 T1omega * \F 2) 22).
+
+Compute ppT1 (standard_gnaw  2 (phi0 T1omega * \F 2) 37).
+
+Compute ppT1 (standard_gnaw  2 (phi0 T1omega * \F 2) 75).
+ 
+*)
 
 
-
-
-
-
-
-
-
-
+Lemma gLemma2_6_1 (alpha:T1) :
+T1nf alpha ->
+  forall beta, T1nf beta -> 
+    T1lt beta  alpha  ->
+    {n:nat | const_pathS n alpha beta}.
+Proof.
+  rewrite -hnf_g2h => Hnf beta Hbeta Hlt.
+  have H: hnf (g2h beta) by rewrite hnf_g2h. 
+  have H'lt : g2h beta t1< g2h alpha; repeat split => //.
+  rewrite T1lt_iff in Hlt => //.
+  case: Hlt => ? [? ?] //.  
+  by rewrite -hnf_g2h. 
+  case (Lemma2_6_1 Hnf H'lt) => x Hx; exists x. 
+  by rewrite /const_pathS. 
+Qed. 
