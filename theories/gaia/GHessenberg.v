1fdd466d31db70ab1172e7afdc969762ed638fc2
Pierre Casteran <pierre.casteran@labri.fr>
2022-02-28 11:05:33 +0100

    gaia-hydras first draft

--- theories/gaia/GHessenberg.v
+++ theories/gaia/GHessenberg.v
@@ -8,14 +8,14 @@ Set Bullet Behavior "Strict Subproofs".
 Require Import T1Bridge. 
 Set Implicit Arguments.
 Unset Strict Implicit.
+Open Scope cantor_scope.
 
+(* begin snippet oplusDef *)
 #[local] Notation hoplus := Epsilon0.Hessenberg.oplus.
 
 Definition oplus alpha beta := h2g (hoplus (g2h alpha) (g2h beta)).
 
-
 Infix "o+" := oplus: cantor_scope. 
-Open Scope cantor_scope.
 
 Fixpoint o_finite_mult n alpha :=
 match n with
@@ -26,9 +26,12 @@ end.
 Compute T1pp (T1omega o+ T1omega).
 
 Compute T1pp (o_finite_mult 5 (T1omega + \F 1)).
+(* end snippet oplusDef *)
+
 
 (** Equations for oplus *)
 
+(* begin snippet oplusEquations:: no-out *)
 Lemma oplus_neutral_l beta : zero o+ beta = beta.
 Proof. rewrite /oplus ; case: beta => // /=.
        move => ? ? ? ;rewrite !h2g_g2hK => //.
@@ -60,8 +63,9 @@ Proof.
   move => t n t0 t1 n0 t2; rewrite !g2h_cons compare_g2h. 
   case (compare t1 t); rewrite h2g_cons !h2g_g2hK; reflexivity. 
 Qed.
+(* end snippet oplusEquations *)
 
-
+(* begin snippet oplusLemmasa:: no-out  *)
 Lemma  oplus_nf (alpha  beta : T1) :
   T1nf alpha ->  T1nf beta -> T1nf (alpha o+ beta).
 Proof.
@@ -69,7 +73,7 @@ Proof.
   apply oplus_nf ; by rewrite hnf_g2h.
 Qed.
 
- Lemma oplus_comm (alpha beta: T1):
+ Lemma oplusC (alpha beta: T1):
    T1nf alpha -> T1nf beta -> alpha o+ beta =  beta o+ alpha.
  Proof.
    move => Halpha Hbeta; rewrite /oplus oplus_comm ?hnf_g2h //.
@@ -78,8 +82,7 @@ Qed.
 
  Lemma oplusA  (alpha beta gamma:T1) :
     T1nf alpha -> T1nf beta -> T1nf gamma ->
-    alpha o+ (beta o+ gamma) =
-      alpha o+ beta o+ gamma.
+    alpha o+ (beta o+ gamma) = alpha o+ beta o+ gamma.
  Proof.
    move => Halpha Hbeta Hgamma;
            rewrite /oplus !g2h_h2gK oplus_assoc ?hnf_g2h => //.
@@ -88,7 +91,8 @@ Qed.
 
  Lemma oplus_lt1 : forall a b, T1nf a -> T1nf b ->  zero <  a ->
                                b < b o+  a.
- Proof.
+   (* end snippet oplusLemmasa *)
+  Proof.
    move => a b Ha Hb Hlt; rewrite /oplus.
    (* Todo : understand contexts use in SSr rewrite *)
    replace b with (h2g (g2h b)) at 1 ; last first.
@@ -97,17 +101,20 @@ Qed.
    rewrite hlt_iff ?h2g_g2hK => //.
  Qed.
 
+  (* begin snippet oplusLemmasb:: no-out  *)
+  
  Lemma oplus_lt2 : forall a b, T1nf a -> T1nf b ->  zero < b ->
                                a < b o+ a.
+  (* end snippet oplusLemmasb  *)
 Proof.
-  intros a b Ha Hb  HD;  rewrite (oplus_comm Hb Ha); auto;
+  intros a b Ha Hb  HD;  rewrite (oplusC Hb Ha); auto;
     apply oplus_lt1;auto.
 Qed.
 
+(* begin snippet oplusLemmasc:: no-out  *)
 Lemma oplus_strict_mono_l (a b c:T1):
-  T1nf a -> T1nf b -> T1nf c ->
-  a < b -> a o+ c < b  o+ c.
-  
+  T1nf a -> T1nf b -> T1nf c ->  a < b -> a o+ c < b  o+ c.
+(* end snippet oplusLemmasc  *)
 Proof.
   rewrite /oplus => Ha Hb Hc.
   rewrite <- hlt_iff => Hab.
@@ -115,10 +122,10 @@ Proof.
   by rewrite hlt_iff ?h2g_g2hK. 
 Qed.
 
-
+(* begin snippet oplusLemmasd:: no-out  *)
 Lemma oplus_strict_mono_r (a b c:T1):
-  T1nf a -> T1nf b -> T1nf c ->
-  b < c -> a o+ b < a  o+ c.
+  T1nf a -> T1nf b -> T1nf c ->  b < c -> a o+ b < a  o+ c.
+(* end snippet oplusLemmasd  *)
 Proof.
    rewrite /oplus => Ha Hb Hc.
   rewrite <- hlt_iff => Hbc.
@@ -126,10 +133,10 @@ Proof.
   by rewrite hlt_iff ?h2g_g2hK.
 Qed.
 
-
+(* begin snippet oplusLemmase:: no-out  *)
 Lemma oplus_lt_phi0 a b c:  T1nf a -> T1nf b -> T1nf c ->
-                                      a < c ->  b < c ->
-                                     phi0 a o+ phi0 b < phi0 c.
+  a < c ->  b < c ->  phi0 a o+ phi0 b < phi0 c.
+(* end snippet oplusLemmase  *)
 Proof.
   move => Ha Hb Hc Hab Hbc; rewrite /oplus.
     replace (phi0 c) with (h2g (hphi0 (g2h c))). 
