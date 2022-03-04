9a1f1f8e72be8b7f675ea86f11cb147a4fd103c9
Pierre Casteran <pierre.casteran@labri.fr>
2022-03-04 16:42:30 +0100

    new file gaia/GHydra.v

---
+++ theories/gaia/GHydra.v
@@ -0,0 +1,20 @@
+From Coq Require Import Logic.Eqdep_dec.
+From hydras Require Import DecPreOrder ON_Generic.
+From hydras Require Import T1 E0 Hessenberg Hydra_Theorems Hydra_Termination.
+From mathcomp Require Import all_ssreflect zify.
+From gaia Require Export ssete9.
+Set Bullet Behavior "Strict Subproofs".
+
+Require Import T1Bridge. 
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Lemma mVariant: Hvariant  nf_Wf free (fun h =>  h2g (m h)).
+Proof. 
+  split; move => i h h' Hnot_head step; rewrite /restrict; split. 
+  1,3 : rewrite -nf_ref; apply m_nf.
+  rewrite -hlt_iff; by apply round_decr.
+Qed.
+
+
