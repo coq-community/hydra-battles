4b162bf9c31f046678d8eb8d5cd6e1e00f57e251
Pierre Casteran <pierre.casteran@labri.fr>
2022-11-04 17:51:32 +0100

    experiment FOL notations

--- theories/ordinals/MoreAck/FOL_notations.v
+++ theories/ordinals/MoreAck/FOL_notations.v
@@ -8,6 +8,14 @@ Delimit Scope fol_scope with fol.
 Infix "=" := (equal _): fol_scope.
 Infix "\/" := (orH _): fol_scope.
 Infix "/\" := (andH _):fol_scope.
+Infix "->" := (impH _): fol_scope.
+Notation "~" := (notH _): fol_scope. 
+Notation atH := (atomic _).
+Notation app1 f arg := (apply _ f (Tcons _ _ arg (Tnil _))).
+Notation app2 f arg1 arg2 := 
+  (apply _ f (Tcons _ _ arg2 (Tcons _ _ arg2 (Tnil _)))).
+Notation allH := (forallH _).
+Notation exH := (forallH _).
 Notation v_ := (var _).
 End FOL_notations.
 
@@ -16,5 +24,5 @@ Section JustTry.
 Variable L: Language.
 Check (var L 1 = var _ 2)%fol.
 Check (v_ 1 = v_ 2)%fol: Formula L.
-
+Check (exH 1 (v_ 1 = v_ 1))%fol : Formula L.
 End JustTry.
