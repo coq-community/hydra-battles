f8b7a955474bf6224ac17740d5f45a2b9702e780
Pierre Casteran <pierre.casteran@labri.fr>
2022-12-15 11:14:07 +0100

    re-structuring proof scripts in Ackermann sub-directory

--- theories/ordinals/Ackermann/NN2PA.v
+++ theories/ordinals/Ackermann/NN2PA.v
@@ -14,91 +14,86 @@ Require Export LNN2LNT.
 Require Import subAll.
 Require Import ListExt.
 
-Lemma NN2PA :
- forall f : fol.Formula LNN,
- folProof.SysPrf LNN NN f -> SysPrf PA (LNN2LNT_formula f). 
+Lemma NN2PA (f : fol.Formula LNN):
+  folProof.SysPrf LNN NN f -> SysPrf PA (LNN2LNT_formula f). 
 Proof.
-intros.
-apply translateProof with NN.
-apply closedPA1.
-intros.
-unfold NN in H0.
-induction H0 as [x H0| x H0].
-induction H0 as [x H0| x H0].
-induction H0 as [x H0| x H0].
-induction H0 as [x H0| x H0].
-induction H0 as [x H0| x H0].
-induction H0 as [x H0| x H0].
-induction H0 as [x H0| x H0].
-induction H0 as [x H0| x H0].
-induction H0.
-apply Axm; repeat (try right; constructor) || left.
-induction H0.
-apply Axm; repeat (try right; constructor) || left.
-induction H0.
-apply Axm; repeat (try right; constructor) || left.
-induction H0.
-apply Axm; repeat (try right; constructor) || left.
-induction H0.
-apply Axm; repeat (try right; constructor) || left.
-induction H0.
-apply Axm; repeat (try right; constructor) || left.
-induction H0.
-apply NN72PA.
-induction H0.
-apply NN82PA.
-induction H0.
-apply NN92PA.
-apply H.
+  intro H; apply translateProof with NN.
+  - apply closedPA1.
+  - intros f0 H0; unfold NN in H0.
+    destruct H0 as [x H0| x H0].
+    + destruct  H0 as [x H0| x H0].
+      * destruct H0 as [x H0| x H0].
+        -- destruct H0 as [x H0| x H0].
+           ++ destruct H0 as [x H0| x H0].
+              ** destruct H0 as [x H0| x H0].
+                 destruct H0 as [x H0| x H0].
+                 destruct H0 as [x H0| x H0].
+                 destruct H0.
+                 apply Axm; repeat (try right; constructor) || left.
+                 destruct H0.
+                 apply Axm; repeat (try right; constructor) || left.
+                 destruct H0.
+                 apply Axm; repeat (try right; constructor) || left.
+                 destruct H0.
+                 apply Axm; repeat (try right; constructor) || left.
+              ** destruct H0.
+                 apply Axm; repeat (try right; constructor) || left.
+           ++ destruct H0.
+              apply Axm; repeat (try right; constructor) || left.
+        -- destruct H0.
+           apply NN72PA.
+      * destruct H0.
+        apply NN82PA.
+    + destruct H0.
+      apply NN92PA.
+  - apply H.
 Qed.
 
 Lemma PAboundedLT :
  forall (m : nat) (a : Formula) (x : nat),
  (forall n : nat,
-  n < m -> SysPrf PA (substituteFormula LNT a x (natToTerm n))) ->
- SysPrf PA (impH (LNN2LNT_formula (LNN.LT (fol.var LNN x) (LNN.natToTerm m))) a).
+     n < m -> SysPrf PA (substituteFormula LNT a x (natToTerm n))) ->
+ SysPrf PA (impH (LNN2LNT_formula (LNN.LT (fol.var LNN x) 
+                                     (LNN.natToTerm m))) 
+              a).
 Proof.
-simple induction m; intros.
-apply impI.
-apply
- contradiction with (LNN2LNT_formula (LNN.LT (fol.var LNN x) (LNN.natToTerm 0))).
-apply Axm; right; constructor.
-apply sysWeaken.
-replace (notH (LNN2LNT_formula (LNN.LT (fol.var LNN x) (LNN.natToTerm 0)))) with
- (LNN2LNT_formula (fol.notH LNN (LNN.LT (fol.var LNN x) (LNN.natToTerm 0)))).
-apply NN2PA.
-apply nn7.
-reflexivity.
-apply impI.
-eapply orE.
-apply impE with (LNN2LNT_formula (LNN.LT (fol.var LNN x) (LNN.natToTerm (S n)))).
-apply sysWeaken.
-assert
- (SysPrf PA
-    (LNN2LNT_formula
-       (LNN.impH (LNN.LT (fol.var LNN x) (LNN.Succ (LNN.natToTerm n)))
-          (LNN.orH (LNN.LT (fol.var LNN x) (LNN.natToTerm n))
-             (LNN.equal (fol.var LNN x) (LNN.natToTerm n)))))).
-apply NN2PA.
-apply nn8.
-simpl in H1.
-simpl in |- *.
-unfold orH, fol.orH in |- *.
-apply H1.
-apply Axm; right; constructor.
-apply sysWeaken.
-simpl in H.
-apply H.
-auto.
-apply sysWeaken.
-apply impI.
-rewrite <- (subFormulaId LNT a x).
-rewrite LNN2LNT_natToTerm.
-apply impE with (substituteFormula LNT a x (natToTerm n)).
-apply (subWithEquals LNT).
-apply eqSym.
-apply Axm; right; constructor.
-apply sysWeaken.
-apply H0.
-apply Nat.lt_succ_diag_r .
+simple induction m. 
+- intros a x H; apply impI.
+  apply contradiction with 
+    (LNN2LNT_formula (LNN.LT (fol.var LNN x) (LNN.natToTerm 0))).
+  apply Axm; right; constructor.
+  apply sysWeaken.
+  replace (notH (LNN2LNT_formula (LNN.LT (fol.var LNN x) (LNN.natToTerm 0)))) 
+    with
+    (LNN2LNT_formula (fol.notH LNN (LNN.LT (fol.var LNN x) (LNN.natToTerm 0)))).
+  + apply NN2PA.
+    apply nn7.
+  + reflexivity.
+- intros n H a x H0; apply impI.
+  eapply orE.
+  + apply impE with 
+      (LNN2LNT_formula (LNN.LT (fol.var LNN x) (LNN.natToTerm (S n)))).
+    * apply sysWeaken.
+      assert
+        (H1: SysPrf PA
+               (LNN2LNT_formula
+                  (LNN.impH (LNN.LT (fol.var LNN x) (LNN.Succ (LNN.natToTerm n)))
+                     (LNN.orH (LNN.LT (fol.var LNN x) (LNN.natToTerm n))
+                        (LNN.equal (fol.var LNN x) (LNN.natToTerm n))))))
+      by (apply NN2PA, nn8). 
+      simpl in H1; simpl. 
+      unfold orH, fol.orH; apply H1.
+    * apply Axm; right; constructor.
+  + apply sysWeaken.
+    simpl in H; apply H; auto. 
+  + apply sysWeaken, impI.
+    rewrite <- (subFormulaId LNT a x).
+    rewrite LNN2LNT_natToTerm.
+    apply impE with (substituteFormula LNT a x (natToTerm n)).
+    * apply (subWithEquals LNT).
+      apply eqSym.
+      apply Axm; right; constructor.
+    * apply sysWeaken.
+      apply H0.
+      apply Nat.lt_succ_diag_r .
 Qed.
