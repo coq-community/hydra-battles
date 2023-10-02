4adf0295a5e151476b57628a9f0671d279934abe
Pierre Castéran <40755850+Casteran@users.noreply.github.com>
2023-10-02 08:15:13 +0200

    Update theories/ordinals/Prelude/ssrnat_extracts.v
    Co-authored-by: Karl Palmskog <palmskog@gmail.com>

--- theories/ordinals/Prelude/ssrnat_extracts.v
+++ theories/ordinals/Prelude/ssrnat_extracts.v
@@ -1,8 +1,5 @@
 (**  * Notations adapted from Mathcomp's ssrnat *)
 
-(* (c) Copyright 2006-2016 Microsoft Corporation and Inria.                  *)
-(* Distributed under the terms of CeCILL-B.                                  *)
-
 Notation "n .+1" := (S n) (at level 2, left associativity,
   format "n .+1") : nat_scope.
 Notation "n .+2" := n.+1.+1 (at level 2, left associativity,
